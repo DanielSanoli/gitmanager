4fe21e84b0e1949efe5776510f2cc81af4ef1204
danielsanoli <daniel.sanoli28@gmail.com>
2025-07-22 21:00:12 -0300

    initial commit

---
+++ src/main/java/br/com/gitmanager/GitManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gitmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitManagerApplication.class, args);
+	}
+
+}
